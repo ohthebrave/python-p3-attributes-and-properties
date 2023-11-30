a57026bfbb0fc5326407db9faaa86e04718a58f1
George <georgejoj28@gmail.com>
2023-11-30 14:08:54 +0300

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,22 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=""):
+        self._name = ""
+        self.name = name  # Use the property setter to validate the name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+
+joe = Dog('jojj')
+# joe.name = 'jojj'
+print(joe.name)
